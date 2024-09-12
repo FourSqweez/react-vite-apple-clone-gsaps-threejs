e180ac8873a5a568bd34397459b8b56203926e30
FourSqweez <jeerasak.kr@chomchob.com>
2024-09-12 16:24:28 +0700

    Add : apply sentry to tracking the app

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,24 @@
-import { StrictMode } from "react";
+import { StrictMode, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import * as Sentry from "@sentry/react";
+import React from "react";
+
+Sentry.init({
+  dsn: "https://4f35dae09355f65747d72d7582a18661@o4507859675119616.ingest.us.sentry.io/4507938982264832",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "four-t7",
+    project: "iphone-gsap-threejs"
+  }), sentryVitePlugin({
+    org: "four-t7",
+    project: "iphone-gsap-threejs"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 })
